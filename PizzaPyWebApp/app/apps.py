c44e45a1b7ee8b4b60cc0ca60053da49502249c6
jackjk521 <jedchu541@gmail.com>
2024-04-04 20:14:35 +0800

    renamed folders

--- PizzaPyWebApp/app/apps.py
+++ PizzaPyWebApp/app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppServerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "app_server"
+    name = "app"
--- PizzaPyWebApp/server/urls.py
+++ PizzaPyWebApp/server/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from app_server import views
+from app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("main/", include("app_server.urls")),
+    path("main/", include("app.urls")),
     path('', views.index, name='index'),
 ]
