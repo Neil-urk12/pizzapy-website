9080a0c894b8325983bfbc5f6f15c547526121ec
jackjk521 <jedchu541@gmail.com>
2024-07-11 20:20:20 +0800

    resolved dynamic events routing

--- PizzaPyWebApp/app/views.py
+++ PizzaPyWebApp/app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-#for Meetup
 import requests
-from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, HttpResponseRedirect
+from django.http import (
+    HttpResponse,
+    HttpRequest,
+    HttpResponseNotFound,
+    HttpResponseRedirect,
+)
 from django.urls import reverse
 from django.conf import settings
 from urllib.parse import urlparse
@@ -10,70 +14,79 @@ from urllib.parse import urlparse
 
 # Create your views here.
 
-# @login_required
+# NAV LINK (PAGES)
 def index(request):
     context = {}
     return render(request, "index.html", context)
 
-def event_page(request):
-    return render(request, 'event_page.html')
+
+def event_page(request, group_name=None):
+    # return render(request, 'event_page.html')
+    return get_upcoming_events(request, group_name)
 
 
 def about_page(request):
-    return render(request, 'about_page.html')
+    return render(request, "about_page.html")
 
+
+# BASE URL SETUP
 def get_redirect_uri(request):
-    default_group_name = 'pizzapy-ph'
+    default_group_name = "pizzapy-ph"
     current_location = request.build_absolute_uri()
 
-    if current_location.startswith('http://127.0.0.1:8001/'):
-        current_location = current_location.replace('http://127.0.0.1:8001/', 'https://pizzapy.ph/')
+    if current_location.startswith("http://127.0.0.1:8001/"):
+        current_location = current_location.replace(
+            "http://127.0.0.1:8001/", "https://pizzapy.ph/"
+        )
 
     parsed_url = urlparse(current_location)
-    path = parsed_url.path.rstrip('/')
-    parts = path.split('/')
+    path = parsed_url.path.rstrip("/")
+    parts = path.split("/")
 
     if len(parts) >= 3:
         if len(parts) == 3:  # URL is like /events/upcoming-events
             parts.append(default_group_name)  # Append default group name
-        redirect_uri = '/'.join(parts)  # Join all parts to form the redirect URI
+        redirect_uri = "/".join(parts)  # Join all parts to form the redirect URI
         return redirect_uri
     else:
         return None  # Unable to determine redirect URI
 
 
-
+# MEETUP TOKEN ACCESS
 def get_access_token(request, code):
     REDIRECT_URI = get_redirect_uri(request)
     if not REDIRECT_URI:
         return None  # Unable to determine redirect URI
-    
-    token_url = 'https://secure.meetup.com/oauth2/access'
+
+    token_url = "https://secure.meetup.com/oauth2/access"
     payload = {
-        'client_id': settings.OAUTH_KEY,
-        'client_secret': settings.OAUTH_SECRET,
-        'grant_type': 'authorization_code',
-        'redirect_uri': REDIRECT_URI,
-        'code': code
+        "client_id": settings.OAUTH_KEY,
+        "client_secret": settings.OAUTH_SECRET,
+        "grant_type": "authorization_code",
+        "redirect_uri": REDIRECT_URI,
+        "code": code,
     }
-    
+
     response = requests.post(token_url, data=payload)
     if response.status_code == 200:
-        return response.json().get('access_token')
+        return response.json().get("access_token")
     else:
         return None
-    
 
+
+# FETCH EVENT VIA QUERY
 def fetch_events(query, token, variables):
-    
-    url = 'https://api.meetup.com/gql'
+
+    url = "https://api.meetup.com/gql"
     headers = {"Authorization": "Bearer " + token}
-    response = requests.post(url, json={"query": query, "variables": variables}, headers=headers)
+    response = requests.post(
+        url, json={"query": query, "variables": variables}, headers=headers
+    )
     if response.status_code == 200:
         return response.json()
     else:
         return None
-    
+
     # Replace with actual fetch logic
     return {
         "data": {
@@ -86,11 +99,15 @@ def fetch_events(query, token, variables):
                                 "title": "Event 1",
                                 "description": "Description of Event 1",
                                 "eventType": "Type 1",
-                                "images": [{"source": "https://via.placeholder.com/748x498.png?text=Event+1"}],
+                                "images": [
+                                    {
+                                        "source": "https://via.placeholder.com/748x498.png?text=Event+1"
+                                    }
+                                ],
                                 "venue": {
                                     "address": "123 Street",
                                     "city": "City 1",
-                                    "postalCode": "12345"
+                                    "postalCode": "12345",
                                 },
                                 "createdAt": "2024-06-01T00:00:00Z",
                                 "dateTime": "2024-06-15T18:00:00Z",
@@ -106,11 +123,11 @@ def fetch_events(query, token, variables):
                                         "id": "1",
                                         "baseUrl": "https://example.com/",
                                         "preview": "https://example.com/preview1.jpg",
-                                        "source": "https://example.com/source1.jpg"
+                                        "source": "https://example.com/source1.jpg",
                                     },
                                     "memberUrl": "https://example.com/member1",
-                                    "organizedGroupCount": 5
-                                }
+                                    "organizedGroupCount": 5,
+                                },
                             }
                         },
                         {
@@ -119,11 +136,15 @@ def fetch_events(query, token, variables):
                                 "title": "Event 2",
                                 "description": "Description of Event 2",
                                 "eventType": "Type 2",
-                                "images": [{"source": "https://via.placeholder.com/748x498.png?text=Event+2"}],
+                                "images": [
+                                    {
+                                        "source": "https://via.placeholder.com/748x498.png?text=Event+2"
+                                    }
+                                ],
                                 "venue": {
                                     "address": "456 Avenue",
                                     "city": "City 2",
-                                    "postalCode": "67890"
+                                    "postalCode": "67890",
                                 },
                                 "createdAt": "2024-06-02T00:00:00Z",
                                 "dateTime": "2024-06-16T18:00:00Z",
@@ -139,11 +160,11 @@ def fetch_events(query, token, variables):
                                         "id": "2",
                                         "baseUrl": "https://example.com/",
                                         "preview": "https://example.com/preview2.jpg",
-                                        "source": "https://example.com/source2.jpg"
+                                        "source": "https://example.com/source2.jpg",
                                     },
                                     "memberUrl": "https://example.com/member2",
-                                    "organizedGroupCount": 10
-                                }
+                                    "organizedGroupCount": 10,
+                                },
                             }
                         },
                         {
@@ -152,11 +173,15 @@ def fetch_events(query, token, variables):
                                 "title": "Event 3",
                                 "description": "Description of Event 3",
                                 "eventType": "Type 2",
-                                "images": [{"source": "https://via.placeholder.com/748x498.png?text=Event+3"}],
+                                "images": [
+                                    {
+                                        "source": "https://via.placeholder.com/748x498.png?text=Event+3"
+                                    }
+                                ],
                                 "venue": {
                                     "address": "456 Avenue",
                                     "city": "City 2",
-                                    "postalCode": "67890"
+                                    "postalCode": "67890",
                                 },
                                 "createdAt": "2024-06-02T00:00:00Z",
                                 "dateTime": "2024-06-16T18:00:00Z",
@@ -172,34 +197,41 @@ def fetch_events(query, token, variables):
                                         "id": "2",
                                         "baseUrl": "https://example.com/",
                                         "preview": "https://example.com/preview2.jpg",
-                                        "source": "https://example.com/source2.jpg"
+                                        "source": "https://example.com/source2.jpg",
                                     },
                                     "memberUrl": "https://example.com/member2",
-                                    "organizedGroupCount": 10
-                                }
+                                    "organizedGroupCount": 10,
+                                },
                             }
-                        }
+                        },
                     ]
                 }
             }
         }
     }
 
+
+# EXTRACT EVENTS TO RENDER
 def extract_events(data, event_timeline):
-    return data.get("data", {}).get("groupByUrlname", {}).get(event_timeline, {}).get("edges", [])
+    return (
+        data.get("data", {})
+        .get("groupByUrlname", {})
+        .get(event_timeline, {})
+        .get("edges", [])
+    )
 
 
+# GET ALL UPCOMING EVENTS API
 def get_upcoming_events(request, group_name=None):
     if not group_name:
         group_name = "pizzapy-ph"
 
     # Simulating failed data retrieval by setting events_json to an empty array and providing an error message
     # return render(request, 'event_page_test2.html', {'error_message': 'Failed to retrieve events', 'events_json': '[]'})
-    
 
     # # Simulating no data received by setting events_json to an empty array
     # return render(request, 'event_page_test2.html', {
-    #     'no_events': 'No upcoming events found', 
+    #     'no_events': 'No upcoming events found',
     #     'events_json': '[]'
     # })
     upcoming_events_query = """
@@ -253,35 +285,49 @@ def get_upcoming_events(request, group_name=None):
     token = request.token
     if not token:
         return HttpResponse("Failed to retrieve access token", status=400)
-    
+
     variables = {"urlname": group_name}
     data = fetch_events(upcoming_events_query, token, variables)
-    
+
     if data:
         events = extract_events(data, "upcomingEvents")
-        # print(events) 
+        # print(events)
         if events:
             first_event = events[0]["node"]
             other_events = [event["node"] for event in events[1:]]
-            return render(request, 'event_page_test2.html', {'first_event': first_event, 'other_events': other_events})
+            return render(
+                request,
+                "event_page_test2.html",
+                {"first_event": first_event, "other_events": other_events},
+            )
         else:
-            return render(request, 'event_page_test2.html', {'error_message': 'No upcoming events found', 'events_json': '[]'})
+            return render(
+                request,
+                "event_page_test2.html",
+                {"error_message": "No upcoming events found", "events_json": "[]"},
+            )
     else:
-        return render(request, 'event_page_test2.html', {'error_message': 'Failed to retrieve events', 'events_json': '[]'})
+        return render(
+            request,
+            "event_page_test2.html",
+            {"error_message": "Failed to retrieve events", "events_json": "[]"},
+        )
 
 
-def event_dispatcher(request, event_timeline, group_name=None):
-    if event_timeline == 'past-events':
-        return #get_past_events(request, group_name)
-    elif event_timeline == 'upcoming-events':
+# EVENT TOGGLE FROM PAST AND UPCOMING EVENTS
+def event_dispatcher(request, event_timeline=None, group_name=None):
+    if event_timeline == "past-events":
+        return  # get_past_events(request, group_name)
+    elif event_timeline == "upcoming-events":
         return get_upcoming_events(request, group_name)
     else:
         return HttpResponseNotFound("Event type not found")
-    
-    
+
+
+# ATTEND EVENT VIA MEETUP API
 def attend_event(request, event_id):
-    if request.method == 'POST':
-        group_name = request.POST.get('group_name')  
-        return HttpResponseRedirect(reverse('get_upcoming_events', args=[group_name]))
+    if request.method == "POST":
+        group_name = request.POST.get("group_name")
+        return HttpResponseRedirect(reverse("get_upcoming_events", args=[group_name]))
     else:
-        return HttpResponse(status=405) 
+        return HttpResponse(status=405)
