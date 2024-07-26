eaff1cb44ef28ca75b4d1788fc187144809e04a2
D. Yu <dyu@fastmail.com>
2024-07-26 19:46:53 +0100

    Add pizzapy.ph and www.pizzapy.ph to settings.ALLOWED_HOSTS

--- PizzaPyWebApp/server/settings.py
+++ PizzaPyWebApp/server/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = "django-insecure-e$-+6ch+!z!ayome!-1=x%w0^db7hbik1@1jkx)gg2u7iffp7(
 DEBUG = True
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.pizzapy.ph", "pizzapy.ph"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
